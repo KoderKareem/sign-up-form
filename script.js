aa402e6366e81613c5f3b7e8eb76040be2743f9c
Koder Kareem <abdulkarimsunusi2001@gmail.com>
2024-03-05 23:24:29 +0100

    The form can now validate password to confirm each field is correct

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const password = document.querySelector("#psw")
-const confirmPassword = document.querySelector("#confirm-psw")
-let incc = document.querySelector(".confirm-psw")
-let incorect = document.querySelector(".incorrect-psw")
-const pswValue =password.addEventListener("input", validate1)
-const confirmPsw = confirmPassword.addEventListener("input", validate2)
-let store;
-function validate1(e){
- store = pswValue;
+const passWordValue = document.querySelector("#psw")
+const confirmpassWordValue = document.querySelector("#confirm-psw")
+let error = document.querySelector(".error")
 
+passWordValue.addEventListener("input", validate)
+confirmpassWordValue.addEventListener("input", validate)
+
+
+
+function validate(e){
+const password1 = passWordValue.value;
+const password2 = confirmpassWordValue.value;
+
+if (password1 !== password2) {
+    error.textContent = "Passwords do not match";
+  } else {
+    error.textContent = "";
+  }
 }
 
-function validate2(e) {
-    if (store != confirmPassword) {
-         
-        incorect.textContent = "password dosen't match"
-        console.log("Incorect Password")
-    } else if(store == confirmPassword){
-       let del = document.querySelector("h6").removeChild()
-    }
-}
